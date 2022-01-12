cf42ff7b18bb31fb38e9006931359574865810ef
Vladislav Kuznetsov <vladkuzodin@gmail.com>
2022-01-12 20:19:40 +0100

    added tinymce editor and react-html-parser module. now can update text and title fields in posts document. there is a bug with how i gather or present data from tinymce.

--- src/components/postLarge.js
+++ src/components/postLarge.js
@@ -1,15 +1,19 @@
 import { useState, useEffect } from "react";
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import Comment from "./comment";
 import CommentForm from './commentForm';
+import PostForm from "./postform";
 import { format } from 'date-fns';
 import parseISO from 'date-fns/parseISO'
 import './styles/postlarge.css';
+import parse from 'html-react-parser';
 
 function PostLarge() {
     const [post, setPost] = useState();
     const [comments, setComments] = useState([]);
     const { id } = useParams();
+    const [updatingPost, setUpdatingPost] = useState(false);
+    const [tinymceKey, setTinymceKey] = useState('');
 
     useEffect(() => {
         fetch(`http://localhost:5000/api/posts/${id}`)
@@ -19,42 +23,51 @@ function PostLarge() {
             setPost(post);
             setComments(post.comments);
         })
-        .catch(err => console.log('There was an error while fetching post: ', err))
+        //DO I NEED IT?
+        // .catch(err => console.log('There was an error while fetching post: ', err));
+        // fetch('http://localhost:5000/api/tinymce-api-key')
+        // .then(res => res.json())
+        // .then(json => setTinymceKey(json.apikey))
+        // .catch(err => console.error(`There was an error while fetching apikey for tinymce editor: ${err}`))
     }, [id, comments]);
 
-    return (
-        <div>
-            {
-            post == null ? 
-            <div>Loading</div>
-            :
-            <div className='main'>
-                <div className='postLarge'>
-                    <h1>{post.title}</h1>
-                    <h2>By {post.author.username}</h2>
-                    <h3>{format(parseISO(post.date), 'do MMMM u, H:m')}</h3>
-                    <article>
-                        {post.text.split('<break>').map(paragraph => {
-                            return (
-                                <p>{paragraph}</p>
-                            )
-                        })}
-                    </article>
+    //render the post
+    if(!updatingPost) {
+        return (
+            <div>
+                {
+                post == null ? 
+                <div>Loading</div>
+                :
+                <div className='main'>
+                    <div className='postLarge'>
+                        <h1>{post.title}</h1>
+                        <h2>By {post.author.username}</h2>
+                        <h3>{format(parseISO(post.date), 'do MMMM u, H:m')}</h3>
+                        {parse(`<article>${post.text}</article>`)}
+                        <button onClick={(e) => setUpdatingPost(!updatingPost)}>Update this post</button>
+                    </div>
+                    <CommentForm />
+                    <h3>Comments:</h3>
+                    {comments.length !== 0 ?
+                     comments.sort((a,b) => {return new Date(b.date) - new Date(a.date)}).map(comment => {
+                         return (
+                            <Comment key={comment._id} comment={comment} />
+                         )
+                    })
+                    : <div>There are no comments yet...</div>
+                    }
                 </div>
-                <CommentForm />
-                <h3>Comments:</h3>
-                {comments.length !== 0 ?
-                 comments.sort((a,b) => {return new Date(b.date) - new Date(a.date)}).map(comment => {
-                     return (
-                        <Comment key={comment._id} comment={comment} />
-                     )
-                })
-                : <div>There are no comments yet...</div>
-                }
-            </div>
-            }  
-        </div>     
-    )
+                }  
+            </div>     
+        )
+    }
+    //render the form
+    else {
+        return (
+            <PostForm updating={updatingPost} setUpdating={setUpdatingPost} post={post} apikey={tinymceKey}/>
+        )
+    }
 }
 
 export default PostLarge
---
+++ src/components/postform.js
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import { Editor } from '@tinymce/tinymce-react';
+import { useNavigate, useParams } from "react-router-dom";
+import { useEffect } from "react/cjs/react.development";
+
+function PostForm (props) {
+    const post = props.post;
+    const apikey = props.apikey;
+    const setUpdating = props.setUpdating;
+    const updating = props.updating;
+    const navigate = useNavigate();
+    const {id} = useParams();
+    const [token, setToken] = useState(localStorage.getItem('authToken'));
+    
+    const [author, setAuthor] = useState(post.author);
+    const [title, setTitle] = useState(post.title);
+    const [text, setText] = useState(post.text);
+    const [date, setDate] = useState(post.date);
+    const [comments, setComments] = useState(post.comments);
+    const [published, setPublished] = useState(post.published);
+
+    const saveChanges = (e) => {
+        e.preventDefault();
+        console.log(text)
+        fetch(`http://localhost:5000/api/posts/${id}`, {
+            method: 'PUT',
+            headers: {
+                'authorization' : `Bearer ${token}`,
+                'Content-Type' : 'application/json'
+            },
+            body: JSON.stringify({author, title, text, date, comments, published})
+        })
+        .then(res => res.json())
+        .then(json => {
+            if (json.success === 'success') {
+                setUpdating(!updating);
+                navigate(`/posts/${id}`)
+            }
+            else {
+                console.log(json.errors)
+            }
+        })
+        .catch(err => console.log('There was an error during PUT operation: ', err))
+    }
+
+    return (
+        <div>
+            <Editor apiKey={apikey} value={text} init={{
+                selector: 'textarea',
+                add_form_submit_trigger : true
+                }} onEditorChange={(value,e) => setText(value)} />
+            <form onSubmit={e => saveChanges(e)}>
+                <label htmlFor='title'>
+                    <input type='text' name='title' defaultValue={title} onChange={e => setTitle(e.target.value)} />
+                </label>
+                <button type='submit'>Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default PostForm
